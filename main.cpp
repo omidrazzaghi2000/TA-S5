2384f11a9b75c648428ad95ce57f096b88d1fdd5
Mohammad Arabzadeh <a.mohamad7824@gmail.com>
2021-10-20 12:31:50 +0330

    added hi to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
-#include <iostream>
 #include "omid.h"
-
+#include <iostream>
 
 int main()
 {
     std::cout << "HI" << std::endl;
     std::cout << "ALL" << std::endl;
     std::cout << "HOW ARE YOU" << std::endl;
+    std::cout << "hi" << std::end;
 
     omid_razzaghi();
 }
