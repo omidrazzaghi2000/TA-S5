faa239d6e510c1a2f1bc871a567ba9dc34a294bd
omiddd <omiddd@NOR>
2021-10-20 12:05:59 +0330

    add omid function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include "omid.h"
+
 
 int main()
 {
     std::cout << "HI" << std::endl;
     std::cout << "ALL" << std::endl;
     std::cout << "HOW ARE YOU" << std::endl;
+
+    omid_razzaghi();
 }
---
+++ omid.h
@@ -0,0 +1,6 @@
+#include <iostream>
+
+void omid_razzaghi()
+{
+    std::cout << "omid razzaghi" << std::endl;
+}
